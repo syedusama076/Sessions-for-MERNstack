eaba6fc472573de8ba5f62f8286503cbd1efab11
Ds-noman <noman.shahid@digiwaresolutions.com>
2025-02-26 22:15:36 +0500

    start javascript

---
+++ script/script.js
@@ -0,0 +1,63 @@
+
+
+// integer
+// 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+
+// float
+// 1.1,2.1,3.1
+
+// string
+// 'Kamran', 'Ali', 'Ahmed', 'usama'
+
+// Boolean
+// true, false
+
+// Array
+// Collection of data of same or different data type
+// 0,1,2,3
+
+
+// types of variable
+
+// let & var
+// const
+
+// Variable Declaration
+// varibale_type variable_name = variable_value
+const arrayVar = [20, 'Ali', true]
+
+// console.log(arrayVar[1])
+
+// Object
+// Collection of key value pair of same or different data type
+const objectVar = {
+    // key:value 
+    name: 'Ali',
+    age: 20,
+    floatingValue: 20.2,
+    isMale: true
+}
+
+
+
+let names = 'Kamaran'
+
+
+
+const age = 20
+
+// names = 'usama'
+
+console.log(names, age)
+
+
+
+
+
+
+
+
+
+
+
+
